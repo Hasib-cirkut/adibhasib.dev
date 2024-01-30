19a01e71832189d70cf2374b10598689fd374ed3
Adib Hasib <adibhasib@pop-os.lan>
2024-01-30 21:23:49 +0600

    Feat: Add ssr support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
+import vercel from '@astrojs/vercel/serverless'
 const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [storyblok({
     accessToken: env.STORYBLOK_TOKEN,
     components: {
